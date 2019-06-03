e5f209c21b53c5a8d99ebdf9f81f58d953c36353
Felix Petriconi <felix@petriconi.net>
2019-06-03 08:13:11 +0200

    Working draft of router

--- stlab/concurrency/router.hpp
+++ stlab/concurrency/router.hpp
@@ -24,103 +24,93 @@ namespace stlab {
 /**************************************************************************************************/
 
 inline namespace v1 {
+
 /**************************************************************************************************/
 
-template<typename T>
+template <typename T>
 using channel_t = std::pair<sender<T>, receiver<T>>;
 
-template <typename E, typename Arg, typename F, typename K>
-class router_ : std::enable_shared_from_this<router_<E, Arg, F, K>>
-{
-    struct concept_t;
-    using route_pair_t = std::pair<const K, std::unique_ptr<concept_t>>;
-    E _executor;
+template <typename T, typename K, typename E, typename F>
+class router_ : public std::enable_shared_from_this<router_<T, K, E, F>> {
+    using route_pair_t = std::pair<K, channel_t<T>>;
+    E _executor; // of the router function
     F _router_function;
     std::vector<route_pair_t> _routes;
-
-    struct concept_t 
-    {
-        virtual void set_ready() = 0;
-        virtual void route(Arg arg) = 0;
-        virtual ~concept_t() = default;
-    };
-
-    template <typename T>
-    struct model : concept_t
-    {
-        model(channel_t<T> c)
-            : _channel(std::move(c))
-        {}
-
-        void set_ready() override {
-            _channel.second.set_ready();
-        }
-
-        void route(Arg arg) override {
-            _channel.first(std::move(arg));
-        }
-
-        channel_t _channel;
-    };
+    bool _ready = false;
 
 public:
-    template <typename... T>
-    router_(E executor, F router_function, route_pair_t<K, channel_t<T>>... route_pairs) 
-    : _executor{std::move(executor)}
-    , _router_function(std::move(router_function))
-    , _routes{ std::make_pair(route_pairs.first,
-               std::make_unique<model<T>>(std::move(route_pairs.second)))... }
-    {}
-    // TODO FP ensure order by key
+    router_() = default;
+    ~router_() = default;
+    router_(router_&&) = default;
+    router_(const router_&) = default;
+    router_& operator=(router_&&) = default;
+    router_& operator=(const router_&) = default;
 
-    void set_ready() {
-        for (auto& route : _routes)
-            route.second->set_ready();
+    router_(E executor, F router_function) :
+        _executor{executor}, _router_function(std::move(router_function)) {}
+
+    template <typename... U>
+    router_(E executor, F router_function, std::pair<K, channel_t<U>>... route_pairs) :
+        _executor{std::move(executor)},
+        _router_function(std::move(router_function)), _routes{
+                                                          std::make_pair(route_pairs.first,
+                                                                         route_pairs.second)...} {
+        set_ready();
     }
 
-    void operator()(Arg arg) {
-        _executor([_weak_this = std::weak_from_this(), _arg = std::move(arg)] {
-            auto smart_this = _weak_this.lock();
-            if (smart_this) {
-                auto keys = smart_this->_router_function(std::move(_arg));
-                auto find_it = std::begin(smart_this->_routes);
-                for (const auto& key : keys) {
-                    find_it =
-                        std::lower_bound(find_it, std::end(smart_this->_routes), key,
-                                         [](const auto& a, const auto& k) { return a.first < k; });
-                    if (find_it == std::end(smart_this->_routes)) return;
-                    if (find_it->first != key) continue;
-                    find_it->second->route(_arg);
-                }
+    void set_ready() {
+        std::sort(_routes.begin(), _routes.end(),
+                  [](const auto& x, const auto& y) { return x.first < y.first; });
+        for (auto& route : _routes)
+            route.second.second.set_ready();
+        _ready = true;
+    }
+
+    stlab::optional<receiver<T>> route(K key) {
+        stlab::optional<receiver<T>> result;
+
+        auto find_it = std::lower_bound(_routes.begin(), _routes.end(), key,
+                                        [](const auto& p, const auto& k) { return p.first < k; });
+
+        if (find_it == _routes.end() || find_it->first != key) return result;
+        result = find_it->second.second;
+
+        return result;
+    }
+
+    template <typename Ex>
+    receiver<T> add_route(K key, Ex executor) {
+        assert(!_ready);
+        assert(!route(key));
+        channel_t<T> ch = channel<T>(executor);
+        auto result = ch.second;
+        _routes.emplace_back(key, std::move(ch));
+        return result;
+    }
+
+    receiver<T> add_route(K key) { return add_route(std::move(key), _executor); }
+
+    void operator()(T arg) {
+        assert(_ready);
+        _executor([_arg = std::move(arg), this] {
+            auto keys = _router_function(_arg);
+            auto find_it = std::begin(_routes);
+            for (const auto& key : keys) {
+                find_it =
+                    std::lower_bound(find_it, std::end(_routes), key,
+                                     [](const auto& p, const auto& k) { return p.first < k; });
+                if (find_it == std::end(_routes)) return;
+                if (find_it->first != key) continue;
+                find_it->second.first(_arg);
             }
         });
     }
 };
 
-template <typename Arg, typename E, typename F, typename K, typename...T>
-auto router(E executor, F router_function, std::pair<const K, channel_t<T>>... route_pairs) {
-    return router_<E, F, Arg, K>(
-               std::move(executor), std::move(router_function), std::move(route_pairs)...);
+template <typename T, typename K, typename E, typename F>
+auto router(E executor, F router_function) {
+    return router_<T, K, E, F>(std::move(executor), std::move(router_function));
 }
-
-template <typename E, typename F, typename I>
-auto router(E executor, F router_function, std::pair<I, I> keys_range) {
-    using route_pair_t = typename std::iterator_traits<I>::value_type;
-    using K = std::remove_const_t<typename route_pair_t::first_type>;
-    using T = typename route_pair_t::second_type;
-    using R = std::vector<route_pair_t>;
-
-    R routes(std::distance(keys_range.first, keys_range.second));
-    std::transform(
-        keys_range.first, keys_range.second, std::begin(routes),
-        [_executor = executor](auto key) { return std::make_pair(key, channel<T>(_executor)); });
-
-    return router_<E, F, K, T, R>(std::move(executor), std::move(router_function),
-                                  std::move(routes));
-}
-
-
-
 
 /**************************************************************************************************/
 
--- test/router_test.cpp
+++ test/router_test.cpp
@@ -9,6 +9,13 @@
 #include <stlab/concurrency/channel.hpp>
 #include <stlab/concurrency/router.hpp>
 #include <stlab/concurrency/default_executor.hpp>
+#include <stlab/concurrency/serial_queue.hpp>
+
+//#include <stlab/concurrency/immediate_executor.hpp>
+
+#include <iostream>
+#include <string>
+#include <thread>
 
 using namespace std;
 using namespace std::placeholders;
@@ -21,7 +28,7 @@ int main() {
     tie(send, receive) = channel<std::string>(default_executor);
 
     // must be in sorted order
-    std::array<std::string, 4 > keys{ "contains hello", "contains world", "default", "hello world" };
+    std::array<std::string, 4> keys{ "contains hello", "contains world", "default", "hello world" };
 
 #if 0
     stlab::router<std::string, std::string> router{ std::make_pair(begin(route_keys), end(route_keys)), [](const std::string& t) {
@@ -33,7 +40,8 @@ int main() {
         return keys;
     } };
 #else
-    auto router = stlab::router(stlab::default_executor, [](const std::string& t) {
+    auto sq = serial_queue_t{default_executor};
+    auto router = stlab::router<std::string, std::string>(sq.executor(), [](const std::string& t) {
         std::vector<std::string> result;
         if (t.find("hello") != std::string::npos) result.push_back("contains hello");
         if (t.find("hello") != std::string::npos) result.push_back("contains hello");
@@ -41,46 +49,41 @@ int main() {
         if (t == "hello world") result.push_back("hello world");
         if (result.empty()) result.push_back("default");
         return result;
-        }, std::make_pair(begin(route_keys), end(route_keys))
-            );
+        }
+        );
 #endif
 
-    std::atomic_bool done{ false };
+    std::atomic_int done{ 4 };
 
-    auto default_case = router.get_route("default") | [](std::string t) -> std::string {
+    auto default_case = router.add_route("default", default_executor) | [](std::string t) -> std::string {
         return t + ": default case\n";
     };
 
-    auto hello = router.get_route("contains hello") | [](std::string t) -> std::string {
+    auto hello = router.add_route("contains hello", default_executor) | [](std::string t) -> std::string {
         return t + ": contains hello\n";
     };
 
-    auto world = router.get_route("contains world") | [](std::string t) -> std::string {
+    auto world = router.add_route("contains world", default_executor) | [](std::string t) -> std::string {
         return t + ": contains world\n";
     };
 
-    auto hello_world = router.get_route("hello world") | [](std::string t) -> std::string {
+    auto hello_world = router.add_route("hello world", default_executor) | [](std::string t) -> std::string {
         return t + ": is hello world\n";
     };
 
-    auto other_hello_world = router.get_route("hello world") | [](std::string t) -> std::string {
-        return t + ": is also hello world\n";
-    };
-
     router.set_ready();
-    auto hold = receive | std::bind(std::move(router), _1);
+    auto hold = receive | std::ref(router);
 
     auto all_cases =
-        stlab::merge_channel<unordered_t>(stlab::default_executor,
-            [&_done = done](std::string message) {
+        stlab::merge_channel<unordered_t>(stlab::default_executor, [&_done = done](std::string message) {
         std::cout << message;
-        _done = true;
-    },
-            std::move(default_case), std::move(hello), std::move(world), std::move(hello_world), std::move(other_hello_world));
+        --_done;
+    }, std::move(default_case), std::move(hello), std::move(world), std::move(hello_world));
 
     // It is necessary to mark the receiver side as ready, when all connections are
     // established
     receive.set_ready();
+    hold.set_ready();
 
     send("bob");
     send("hello");
@@ -88,8 +91,8 @@ int main() {
     send("hello world");
 
     // Waiting just for illustrational purpose
-    while (!done.load()) {
-        this_thread::sleep_for(chrono::milliseconds(1));
+    while (done.load() != 0) {
+        std::this_thread::sleep_for(chrono::milliseconds(1));
     }
     return 0;
 }
